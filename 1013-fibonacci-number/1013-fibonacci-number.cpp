9653b15e2eac4a0a2c680adb8c3de9aa523363bb
munukutlapraveen88 <munukutlapraveen88@gmail.com>
2025-04-25 16:52:07 +0530

    Time: 0 ms (100%), Space: 7.8 MB (37.38%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,7 +1,9 @@
 class Solution {
+    vector<int> cache = vector<int>(36, -1);
 public:
     int fib(int n) {
         if(n <= 1) return n;
-        return fib(n-1) + fib(n-2);
+        if(cache[n]!=-1)return cache[n];
+        return cache[n] = fib(n-1) + fib(n-2);
     }
 };
