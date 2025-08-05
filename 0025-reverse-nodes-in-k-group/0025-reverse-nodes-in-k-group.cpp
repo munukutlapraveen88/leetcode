4096af8692d734c5cdba40cb637db0e0b3995286
munukutlapraveen88 <munukutlapraveen88@gmail.com>
2025-08-05 17:00:19 +0530

    Time: 0 ms (100%), Space: 16.4 MB (71.2%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
+    bool nothavekNodes(ListNode* head, int k){
+        int cnt =0;
+        while(head &&  cnt < k){
+            head = head ->next;
+            ++cnt;
+        }
+        return cnt == k;
+    }
+    
+  pair<ListNode*, ListNode*>  reverseKnodes(ListNode* head, int k){
+    
+    ListNode* prev = nullptr;
+    ListNode*  curr = head;
+    for(int i =0 ; i<k ; ++i){
+        ListNode* nextNode = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nextNode;
+        
+    }
+    return {prev, curr};
+  }
 
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if(k == 1 || head == NULL) return head;
-        
-        ListNode* temp = head;
-        int cnt = 0;
-        while(temp && cnt < k){
-            temp = temp->next;
-            cnt++;
-        }
-        if(cnt < k) return head;
-        ListNode* prev = nullptr;
-        ListNode* curr = head;
-        ListNode* next = nullptr;
-        int count = 0;
-        while(curr && count < k){
-            next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
-            count++;
-        }
-        if(next){
-            head->next = reverseKGroup(next, k);
-        }
-        return prev;
+    if(!head || k <= 1 || !nothavekNodes(head, k)) return head;
+    
+    auto [newHead, nextpart] = reverseKnodes(head, k);
+    
+    head -> next = reverseKGroup(nextpart, k);
+    
+    return newHead;
+    
+    
     }
 };
